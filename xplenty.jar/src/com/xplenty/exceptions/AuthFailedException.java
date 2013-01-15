08c9e3f48ee895b43e7a300c418a3f2993021983
reverendk <yuriy.kovalek@gmail.com>
2013-01-15 17:35:30 +0200

    Implemented methods: list cluster plans, list clusters, show cluster info, list jobs, show job info. First sketch of exception hierarchy and handling.

---
+++ xplenty.jar/src/com/xplenty/exceptions/AuthFailedException.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.xplenty.exceptions;
+
+
+/**
+ * @author Yuriy Kovalek
+ *
+ */
+public class AuthFailedException extends RequestFailedException {
+	private static final long serialVersionUID = 3015805619788286689L;
+
+	public AuthFailedException(String msg, int status, String response, String apiKey) {
+		super("API key <" + apiKey + ">: " + msg, status, response);
+	}
+}
