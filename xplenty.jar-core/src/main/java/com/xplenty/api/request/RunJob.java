1fe1a580b78d04ac0d9057b5d8751ad43a3e5740
xardas <xardas@rutarget.ru>
2015-12-20 20:36:08 +0300

    some refactoring

--- xplenty.jar-core/src/main/java/com/xplenty/api/request/RunJob.java
+++ xplenty.jar-core/src/main/java/com/xplenty/api/request/RunJob.java
@@ -3,31 +3,21 @@
  */
 package com.xplenty.api.request;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sun.jersey.api.client.ClientResponse;
 import com.xplenty.api.Xplenty;
-import com.xplenty.api.exceptions.XplentyAPIException;
 import com.xplenty.api.model.Job;
 import com.xplenty.api.util.Http;
-import com.xplenty.api.util.Http.MediaType;
 import com.xplenty.api.util.Http.Method;
 
 /**
  * @author Yuriy Kovalek
  *
  */
-public class RunJob implements Request<Job> {
-	private final Job job;
-	
-	public RunJob(Job job) {
-		this.job = job;
-	}
-	
-	@Override
+public class RunJob extends AbstractManipulationRequest<Job> {
+    protected RunJob(Job entity) {
+        super(entity);
+    }
+
+    @Override
 	public String getName() {
 		return Xplenty.Resource.RunJob.name;
 	}
@@ -38,34 +28,12 @@ public class RunJob implements Request<Job> {
 	}
 
 	@Override
-	public MediaType getResponseType() {
-		return Http.MediaType.JSON;
-	}
-
-	@Override
 	public String getEndpoint() {
 		return Xplenty.Resource.RunJob.value;
 	}
 
-	@Override
-	public boolean hasBody() {
-		return true;
-	}
-
-	@Override
-	public Object getBody() {
-		Map<String, Object> j = new HashMap<String, Object>();
-		j.put("job", job);
-		return j;
-	}
-
-	@Override
-	public Job getResponse(ClientResponse response) {
-		String json = response.getEntity(String.class);		
-		try {
-			return new ObjectMapper().readValue(json, new TypeReference<Job>() {});
-		} catch (Exception e) {
-			throw new XplentyAPIException(getName() + ": error parsing response object", e);
-		}
-	}
+    @Override
+    protected String getPackKey() {
+        return "job";
+    }
 }
--- xplenty.jar-core/src/main/java/com/xplenty/api/request/StopJob.java
+++ xplenty.jar-core/src/main/java/com/xplenty/api/request/StopJob.java
@@ -3,64 +3,26 @@
  */
 package com.xplenty.api.request;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sun.jersey.api.client.ClientResponse;
 import com.xplenty.api.Xplenty;
-import com.xplenty.api.exceptions.XplentyAPIException;
 import com.xplenty.api.model.Job;
-import com.xplenty.api.util.Http;
-import com.xplenty.api.util.Http.MediaType;
-import com.xplenty.api.util.Http.Method;
 
 /**
  * @author Yuriy Kovalek
  *
  */
-public class StopJob implements Request<Job> {
-	private final long jobId;
-	
-	public StopJob(long jobId) {
-		this.jobId = jobId;
-	}
+public class StopJob extends AbstractDeleteRequest<Job> {
 
-	@Override
+    protected StopJob(Long entityId) {
+        super(entityId);
+    }
+
+    @Override
 	public String getName() {
 		return Xplenty.Resource.StopJob.name;
 	}
 
 	@Override
-	public Method getHttpMethod() {
-		return Http.Method.DELETE;
-	}
-
-	@Override
-	public MediaType getResponseType() {
-		return Http.MediaType.JSON;
-	}
-
-	@Override
 	public String getEndpoint() {
-		return Xplenty.Resource.StopJob.format(Long.toString(jobId));
-	}
-
-	@Override
-	public boolean hasBody() {
-		return false;
-	}
-
-	@Override
-	public Job getBody() {
-		return null;
-	}
-
-	@Override
-	public Job getResponse(ClientResponse response) {
-		String json = response.getEntity(String.class);		
-		try {
-			return new ObjectMapper().readValue(json, new TypeReference<Job>() {});
-		} catch (Exception e) {
-			throw new XplentyAPIException(getName() + ": error parsing response object", e);
-		}
+		return Xplenty.Resource.StopJob.format(Long.toString(entityId));
 	}
 }
