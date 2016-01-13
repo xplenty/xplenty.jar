3c13f2d7781e08638c56b13a64159cfd205e478b
xardas <xardas@rutarget.ru>
2016-01-13 19:37:05 +0300

    merge modules renew dependencies some bug fixes including: -correct handling of 204 status by jersey client -add watchers sending null as body

---
+++ src/main/java/com/xplenty/api/request/watching/AddClusterWatcher.java
@@ -0,0 +1,48 @@
+package com.xplenty.api.request.watching;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.xplenty.api.exceptions.XplentyAPIException;
+import com.xplenty.api.http.Http;
+import com.xplenty.api.http.Response;
+import com.xplenty.api.model.ClusterWatchingLogEntry;
+import com.xplenty.api.request.AbstractRequest;
+
+import static com.xplenty.api.Xplenty.Resource;
+import static com.xplenty.api.http.Http.Method.POST;
+
+public class AddClusterWatcher extends AbstractRequest<ClusterWatchingLogEntry> {
+    private Long _clusterId;
+
+    public AddClusterWatcher(Long subjectId) {
+        _clusterId = subjectId;
+    }
+
+    @Override
+    public String getName() {
+        return Resource.ClusterWatcher.name;
+    }
+
+    @Override
+    public Http.Method getHttpMethod() {return POST; }
+
+    @Override
+    public Http.MediaType getResponseType()  { return Http.MediaType.JSON; }
+
+    @Override
+    public String getEndpoint() { return Resource.ClusterWatcher.format(Long.toString(_clusterId)); }
+
+    @Override
+    public boolean hasBody() { return false; }
+
+    @Override
+    public Object getBody() { return null; }
+
+    @Override
+    public ClusterWatchingLogEntry getResponse(Response response) {
+        try {
+            return response.getContent(new TypeReference<ClusterWatchingLogEntry>() {});
+        } catch (Exception e) {
+            throw new XplentyAPIException(getName() + ": error parsing response object", e);
+        }
+    }
+}
