71c9c074b4c4410be7822581642ca2e832ac6f60
xardas <xardasex@gmail.com>
2016-02-26 23:41:44 +0300

    add comments for job output

--- src/main/java/com/xplenty/api/model/JobOutput.java
+++ src/main/java/com/xplenty/api/model/JobOutput.java
@@ -49,40 +49,93 @@ public class JobOutput extends XplentyObject<JobOutput> {
         super(JobOutput.class);
     }
 
+    /**
+     *
+     * @return Preview (up to 100 rows) of this job output
+     */
     public JobOutputPreview getPreview() {
         if (this.getParentApiInstance() == null) {
             throw new XplentyAPIException("The parent API instance is not set");
         }
         return getParentApiInstance().previewJobOutput(jobId, id);
     }
-	
+
+    /**
+     *
+     * @return job output id
+     */
 	public Long getId() {
 		return id;
 	}
+
+    /**
+     *
+     * @return job output name (filename, link, etc)
+     */
 	public String getName() {
 		return name;
 	}
+
+    /**
+     *
+     * @return number of records in this job output
+     */
 	public Long getRecordsCount() {
 		return recordsCount;
 	}
+
+    /**
+     *
+     * @return job output progress
+     */
 	public Double getProgress() {
 		return progress;
 	}
+
+    /**
+     *
+     * @return component object (related to package destination component)
+     */
     public Component getComponent() {
         return component;
     }
+
+    /**
+     *
+     * @return date of creation
+     */
     public Date getCreatedAt() {
 		return createdAt;
 	}
+
+    /**
+     *
+     * @return date of update
+     */
 	public Date getUpdatedAt() {
 		return updatedAt;
 	}
+
+    /**
+     *
+     * @return Resource URL for job output preview
+     */
 	public String getPreviewUrl() {
 		return previewUrl;
 	}
+
+    /**
+     *
+     * @return type of preview (json, csv etc)
+     */
     public String getPreviewType() {
         return previewType;
     }
+
+    /**
+     *
+     * @return Resource URL for this job output
+     */
     public String getUrl() {
         return url;
     }
@@ -98,14 +151,26 @@ public class JobOutput extends XplentyObject<JobOutput> {
         protected Component() {
         }
 
+        /**
+         *
+         * @return component name
+         */
         public String getName() {
             return name;
         }
 
+        /**
+         *
+         * @return component type
+         */
         public String getType() {
             return type;
         }
 
+        /**
+         *
+         * @return output fields
+         */
         public List<String> getFields() {
             return fields;
         }
